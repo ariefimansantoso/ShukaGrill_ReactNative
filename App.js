ea2db8433ade14d2e2ec16bae29c13a0e1d9ff40
Arief Santoso <ariefimansantoso@gmail.com>
2023-12-10 11:02:08 +0700

    remove sentry

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 import AppNavigation from './src/navigation/AppNavigation';
+//import * as Sentry from '@sentry/react-native';
+
+//Sentry.init({
+//  dsn: 'https://829d4c81be10d55e97e20025726be40f@o4506362332184576.ingest.sentry.io/4506362334478336',
+//});
 
 const App = () => {
   return <AppNavigation />;
